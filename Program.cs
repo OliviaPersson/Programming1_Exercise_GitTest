b31d6989b9ac4c6fa4ccc588b82dec97796cea69
Olivia Persson <Olivia__P@hotmail.com>
2020-10-22 10:10:36 +0200

    Nostalgiloopen tillagd

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ namespace Programming1_Exercise_GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            //Console.WriteLine("Hello world!");
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}^2 = {1}", i, i * i);
+                Console.ReadKey();
+            }
 
         }
     }

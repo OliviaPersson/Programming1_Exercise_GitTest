6463b9d59561100ba0c26f81fbc69e80b7294020
Olivia Persson <Olivia__P@hotmail.com>
2020-10-22 10:47:10 +0200

    Enhanced loop printout with cube

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Programming1_Exercise_GitTest
             //Console.WriteLine("Hello world!");
             for(int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("{0}^2 = {1}", i, i * i);
+                Console.WriteLine("{0}^2 = {1}, {0}^3 = {2}", i, i*i, i*i*i);
                 Console.ReadKey();
             }
 
